0bc545fdcebbe46077c465ba1534ee0b5f90b410
GeometricEath <geo.fsv@gmail.com>
2023-03-13 10:52:16 +0300

    feat: реализовать добавление начальных карточек из JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
+
 const profile = document.querySelector('.profile');
 const userName = profile.querySelector('.profile__user-name');
 const userOccupation = profile.querySelector('.profile__occupation');
@@ -9,6 +36,9 @@ const profileModalCloseButton =
 const profileModalForm = document.forms.profileModalForm;
 const userOccupationModalFild = profileModalForm.userOccupation;
 const userNameModalFild = profileModalForm.userName;
+
+const templateCard = document.querySelector('#templateCard').content;
+const galleryContainer = document.querySelector('.gallery__card-list');
 
 editProfileButton.addEventListener('click', showEditProfileModal);
 profileModalForm.addEventListener('submit', editProfile);
@@ -33,3 +63,16 @@ function editProfile(event) {
   userOccupation.textContent = userOccupationModalFild.value;
   togglePopUp(profileModal);
 }
+
+function renderDefaultCards(elementsArray) {
+  initialCards.forEach((item) => addCard(item));
+}
+
+function addCard(item) {
+  const cardElement = templateCard.cloneNode(true);
+  cardElement.querySelector('.card__image').setAttribute('src', item.link);
+  cardElement.querySelector('.card__title').innerText = item.name;
+  galleryContainer.prepend(cardElement);
+}
+
+renderDefaultCards(initialCards);
