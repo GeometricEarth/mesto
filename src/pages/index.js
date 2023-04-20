90b249170271abd5771e978547cf200ab76ae346
GeometricEath <geo.fsv@gmail.com>
2023-04-20 16:49:40 +0300

    Squashed commit of the following:
    commit 7d93284e1b1b5f73bb782214ef1e222a06a3b10f
    Author: GeometricEath <geo.fsv@gmail.com>
    Date:   Thu Apr 20 16:48:45 2023 +0300
    
        feat: реализовать передачу данных из userInfo в попап редактированая профиля
    
    commit cfa632d806122f2d176dcd2fd2eec2c4320830de
    Author: GeometricEath <geo.fsv@gmail.com>
    Date:   Thu Apr 20 16:47:22 2023 +0300
    
        feature: реализовать класс userInfo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,29 @@
 import './index.css';
+import { initialCards } from '../utils/defaultsCards.js';
+import { validationConfig } from '../utils/validationConfig.js';
+
 import Section from '../components/Section.js';
 import Card from '../components/Card.js';
-import { initialCards } from '../utils/defaultsCards.js';
+import UserInfo from '../components/UserInfo';
 import FormValidator from '../components/FormValidator.js';
-import { validationConfig } from '../utils/validationConfig.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 
-const profile = document.querySelector('.profile');
-const userName = profile.querySelector('.profile__user-name');
-const userOccupation = profile.querySelector('.profile__occupation');
-const buttonEditingProfile = profile.querySelector('.button_type_edit');
+// const profile = document.querySelector('.profile');
+// const userName = profile.querySelector('.profile__user-name');
+// const userOccupation = profile.querySelector('.profile__occupation');
+const buttonEditingProfile = document.querySelector('.button_type_edit');
 
 const galleryContainer = document.querySelector('.gallery__card-list');
 
 const profilePopuplForm = document.forms.profileModalForm;
-const userOccupationModalFild = profilePopuplForm.userOccupation;
-const userNameModalFild = profilePopuplForm.userName;
+// const userOccupationModalFild = profilePopuplForm.userOccupation;
+// const userNameModalFild = profilePopuplForm.userName;
 
 const newPlaceForm = document.forms.newPlaceForm;
-const buttonShowNewPlacePopup = profile.querySelector('.button_type_add');
+const buttonShowNewPlacePopup = document.querySelector('.button_type_add');
+
+const userInfo = new UserInfo('.profile__user-name', '.profile__occupation');
 
 const profilePopuplFormValidate = new FormValidator(validationConfig, profilePopuplForm);
 const newPlaceFormValidate = new FormValidator(validationConfig, newPlaceForm);
@@ -36,24 +40,24 @@ popupWithImage.setEventListeners();
 newPlacePopup.setEventListeners();
 profilePopup.setEventListeners();
 
-const profileData = {
-  userName: userName.textContent,
-  userOccupation: userOccupation.textContent,
-};
-profilePopup.setInputValues(profileData);
+// const profileData = {
+//   userName: userName.textContent,
+//   userOccupation: userOccupation.textContent,
+// };
+// profilePopup.setInputValues(profileData);
 
-function handleShowEditProfileModal() {
-  userOccupationModalFild.value = userOccupation.textContent;
-  userNameModalFild.value = userName.textContent;
-  profilePopuplFormValidate.resetValidation(true);
-  // openPopup(profilePopup);
-}
+// function handleShowEditProfileModal() {
+//   userOccupationModalFild.value = userOccupation.textContent;
+//   userNameModalFild.value = userName.textContent;
+//   profilePopuplFormValidate.resetValidation(true);
+//   // openPopup(profilePopup);
+// }
 
-function handleSaveNewProfileData(data) {
-  userName.textContent = userNameModalFild.value;
-  userOccupation.textContent = userOccupationModalFild.value;
-  // closePopup(profilePopup);
-}
+// function handleSaveNewProfileData(data) {
+//   userName.textContent = userNameModalFild.value;
+//   userOccupation.textContent = userOccupationModalFild.value;
+//   // closePopup(profilePopup);
+// }
 
 function handleCardClick(placeName, placeImage) {
   popupWithImage.open(placeName, placeImage);
@@ -71,6 +75,7 @@ function renderCard(data) {
 renderDefaultCards(initialCards);
 
 buttonEditingProfile.addEventListener('click', () => {
+  profilePopup.setInputValues(userInfo.getUserInfo());
   profilePopup.open();
 });
 
