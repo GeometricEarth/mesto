37b5836108e19dd5af4d7c5b677ff4fb0cf2e987
GeometricEath <geo.fsv@gmail.com>
2023-04-20 16:54:25 +0300

    Squashed commit of the following:
    commit 90b249170271abd5771e978547cf200ab76ae346
    Author: GeometricEath <geo.fsv@gmail.com>
    Date:   Thu Apr 20 16:49:40 2023 +0300
    
        Squashed commit of the following:
    
        commit 7d93284e1b1b5f73bb782214ef1e222a06a3b10f
        Author: GeometricEath <geo.fsv@gmail.com>
        Date:   Thu Apr 20 16:48:45 2023 +0300
    
            feat: реализовать передачу данных из userInfo в попап редактированая профиля
    
        commit cfa632d806122f2d176dcd2fd2eec2c4320830de
        Author: GeometricEath <geo.fsv@gmail.com>
        Date:   Thu Apr 20 16:47:22 2023 +0300
    
            feature: реализовать класс userInfo
    
    commit 97f24bd8e0559d83a8895f5dafebef6b63576b29
    Author: GeometricEath <geo.fsv@gmail.com>
    Date:   Thu Apr 20 16:08:34 2023 +0300
    
        Squashed commit of the following:
    
        commit 4dde2e84e42069d0f2de102943cd8534d4cc8578
        Author: GeometricEath <geo.fsv@gmail.com>
        Date:   Thu Apr 20 15:51:18 2023 +0300
    
            ref: открытие попапа профиля
    
        commit ba403b3615cab377b0d9a6d8ba25baffdb1a9a7a
        Author: GeometricEath <geo.fsv@gmail.com>
        Date:   Thu Apr 20 15:50:26 2023 +0300
    
            fix: исправить обработку объекта в методе setInputValuts
    
        commit f2d09f0e17acdeeda14a5511dc183fcfd0ce2c31
        Author: GeometricEath <geo.fsv@gmail.com>
        Date:   Thu Apr 20 15:49:23 2023 +0300
    
            fix: исправить имя пользователя
    
        commit aa57528652012f5202da6fd1f6ecefbb97ca094c
        Author: GeometricEath <geo.fsv@gmail.com>
        Date:   Thu Apr 20 13:09:19 2023 +0300
    
            ref: реализовать обработку субмита формы нового места
    
        commit 3b619112f72a54de064cf9647b5a9ea266d7fe3f
        Author: GeometricEath <geo.fsv@gmail.com>
        Date:   Wed Apr 19 12:42:08 2023 +0300
    
            ref: перенести логику попапа с изображением в отдельный класс

---
+++ src/components/UserInfo.js
@@ -0,0 +1,20 @@
+export default class UserInfo {
+  constructor(nameSelector, occupationSelector) {
+    this._nameElement = document.querySelector(nameSelector);
+    this._occupationElement = document.querySelector(occupationSelector);
+  }
+
+  getUserInfo() {
+    const state = {
+      userName: this._nameElement.textContent,
+      userOccupation: this._occupationElement.textContent,
+    };
+
+    return state;
+  }
+
+  setUserInfo({ userName, userOcupation }) {
+    this._nameElement.textContent = userName;
+    this._occupationElement.textContent = userOcupation;
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,31 @@
-import './index.css';
 import Section from '../components/Section.js';
 import Card from '../components/Card.js';
-import { initialCards } from '../utils/defaultsCards.js';
 import FormValidator from '../components/FormValidator.js';
+import { initialCards } from '../utils/defaultsCards.js';
 import { validationConfig } from '../utils/validationConfig.js';
-import PopupWithImage from '../components/PopupWithImage.js';
-import PopupWithForm from '../components/PopupWithForm.js';
+import './index.css';
 
 const profile = document.querySelector('.profile');
 const userName = profile.querySelector('.profile__user-name');
 const userOccupation = profile.querySelector('.profile__occupation');
 const buttonEditingProfile = profile.querySelector('.button_type_edit');
 
-const galleryContainer = document.querySelector('.gallery__card-list');
+const popupList = document.querySelectorAll('.popup');
+const buttonsClosePopupList = document.querySelectorAll('.button_type_close');
 
+const profilePopup = document.querySelector('.popup_type_edit-profile');
 const profilePopuplForm = document.forms.profileModalForm;
 const userOccupationModalFild = profilePopuplForm.userOccupation;
 const userNameModalFild = profilePopuplForm.userName;
 
+const galleryContainer = document.querySelector('.gallery__card-list');
+
+const enlargedImagePopup = document.querySelector('.popup_type_image-scaling');
+const enlargedImage = enlargedImagePopup.querySelector('.popup__enlarged-image');
+
+const newPlacePopup = document.querySelector('.popup_type_add-place');
 const newPlaceForm = document.forms.newPlaceForm;
+const newPlaseFieldList = newPlaceForm.querySelectorAll('.popup__field');
 const buttonShowNewPlacePopup = profile.querySelector('.button_type_add');
 
 const profilePopuplFormValidate = new FormValidator(validationConfig, profilePopuplForm);
@@ -26,37 +33,55 @@ const newPlaceFormValidate = new FormValidator(validationConfig, newPlaceForm);
 profilePopuplFormValidate.enableValidation();
 newPlaceFormValidate.enableValidation();
 
-const profilePopup = new PopupWithForm('.popup_type_edit-profile', (data) => {});
-const newPlacePopup = new PopupWithForm('.popup_type_add-place', (data) => {
-  renderCard(data);
-  newPlacePopup.close();
-});
-const popupWithImage = new PopupWithImage('.popup_type_image-scaling');
-popupWithImage.setEventListeners();
-newPlacePopup.setEventListeners();
-profilePopup.setEventListeners();
-
-const profileData = {
-  userName: userName.textContent,
-  userOccupation: userOccupation.textContent,
-};
-profilePopup.setInputValues(profileData);
-
-function handleShowEditProfileModal() {
+function handleShowEditProfileModal(event) {
+  event.preventDefault();
   userOccupationModalFild.value = userOccupation.textContent;
   userNameModalFild.value = userName.textContent;
   profilePopuplFormValidate.resetValidation(true);
-  // openPopup(profilePopup);
+  openPopup(profilePopup);
 }
 
-function handleSaveNewProfileData(data) {
+function handleSaveNewProfileData(event) {
+  event.preventDefault();
   userName.textContent = userNameModalFild.value;
   userOccupation.textContent = userOccupationModalFild.value;
-  // closePopup(profilePopup);
+  closePopup(profilePopup);
 }
 
-function handleCardClick(placeName, placeImage) {
-  popupWithImage.open(placeName, placeImage);
+function showEnlargedImagePopup(placeName, placeImage) {
+  enlargedImage.setAttribute('src', placeImage);
+  enlargedImage.setAttribute('alt', placeName);
+  enlargedImagePopup.querySelector('.popup__place-title').innerText = placeName;
+  openPopup(enlargedImagePopup);
+}
+
+function handleAddPlace(event) {
+  event.preventDefault();
+  const newPlace = {};
+  newPlaseFieldList.forEach((item) => {
+    newPlace[item.name] = item.value;
+  });
+  renderCard(newPlace);
+
+  closePopup(newPlacePopup);
+  newPlaceForm.reset();
+}
+
+function openPopup(element) {
+  element.classList.add('popup_opened');
+  document.addEventListener('keydown', handleEscapeKeyListener);
+}
+
+function closePopup(element) {
+  element.classList.remove('popup_opened');
+  document.removeEventListener('keydown', handleEscapeKeyListener);
+}
+
+function handleEscapeKeyListener(evt) {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_opened');
+    closePopup(openedPopup);
+  }
 }
 
 function renderDefaultCards(elementsArray) {
@@ -70,8 +95,20 @@ function renderCard(data) {
 
 renderDefaultCards(initialCards);
 
-buttonEditingProfile.addEventListener('click', () => {
-  profilePopup.open();
+//назначения обработчиков клика за пределы формы
+popupList.forEach((element) => {
+  element.addEventListener('click', (event) => {
+    const targegClassList = event.target.classList;
+    if (targegClassList.contains('popup')) closePopup(element);
+  });
+});
+
+//Назначение обработчиков клика по кнопкам закрытия попапов
+buttonsClosePopupList.forEach((button) => {
+  const targetPopup = button.closest('.popup');
+  button.addEventListener('click', () => {
+    closePopup(targetPopup);
+  });
 });
 
 buttonShowNewPlacePopup.addEventListener('click', () => {
