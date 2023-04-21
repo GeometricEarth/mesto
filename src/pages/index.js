faaeef9bb599b96be106603b2c12d39893eeca7f
GeometricEath <geo.fsv@gmail.com>
2023-04-21 09:18:06 +0300

    style: удалены неиспользуемые фрагменты кода, перенесены константы в отдельный файл

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,67 +1,51 @@
 import './index.css';
 import { initialCards } from '../utils/defaultsCards.js';
-import { validationConfig } from '../utils/validationConfig.js';
+import {
+  validationConfig,
+  userNameSelector,
+  userOccupationSelector,
+  gallerySelector,
+  profilePopupSelector,
+  newPlacePopupSelector,
+  popupWithImageSelector,
+  cardTemplateSelector,
+  buttonOpenProfilePopupSelector,
+  buttonOpenNewPlacePopupSelector,
+} from '../utils/constants.js';
 
+import UserInfo from '../components/UserInfo';
 import Section from '../components/Section.js';
 import Card from '../components/Card.js';
-import UserInfo from '../components/UserInfo';
 import FormValidator from '../components/FormValidator.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 
-// const profile = document.querySelector('.profile');
-// const userName = profile.querySelector('.profile__user-name');
-// const userOccupation = profile.querySelector('.profile__occupation');
-const buttonEditingProfile = document.querySelector('.button_type_edit');
-
-const gallerySelector = '.gallery__card-list';
+const buttonEditingProfile = document.querySelector(buttonOpenProfilePopupSelector);
+const buttonShowNewPlacePopup = document.querySelector(buttonOpenNewPlacePopupSelector);
 
 const profilePopuplForm = document.forms.profileModalForm;
-// const userOccupationModalFild = profilePopuplForm.userOccupation;
-// const userNameModalFild = profilePopuplForm.userName;
-
 const newPlaceForm = document.forms.newPlaceForm;
-const buttonShowNewPlacePopup = document.querySelector('.button_type_add');
 
-const userInfo = new UserInfo('.profile__user-name', '.profile__occupation');
+const userInfo = new UserInfo(userNameSelector, userOccupationSelector);
 
 const profilePopuplFormValidate = new FormValidator(validationConfig, profilePopuplForm);
 const newPlaceFormValidate = new FormValidator(validationConfig, newPlaceForm);
 profilePopuplFormValidate.enableValidation();
 newPlaceFormValidate.enableValidation();
 
-const profilePopup = new PopupWithForm('.popup_type_edit-profile', (data) => {
+const profilePopup = new PopupWithForm(profilePopupSelector, (data) => {
   userInfo.setUserInfo(data);
   profilePopup.close();
 });
 
-const newPlacePopup = new PopupWithForm('.popup_type_add-place', (data) => {
+const newPlacePopup = new PopupWithForm(newPlacePopupSelector, (data) => {
   renderCard(data);
   newPlacePopup.close();
 });
-const popupWithImage = new PopupWithImage('.popup_type_image-scaling');
+const popupWithImage = new PopupWithImage(popupWithImageSelector);
 popupWithImage.setEventListeners();
 newPlacePopup.setEventListeners();
 profilePopup.setEventListeners();
-
-// const profileData = {
-//   userName: userName.textContent,
-//   userOccupation: userOccupation.textContent,
-// };
-// profilePopup.setInputValues(profileData);
-
-// function handleShowEditProfileModal() {
-//   userOccupationModalFild.value = userOccupation.textContent;
-//   userNameModalFild.value = userName.textContent;
-//   profilePopuplFormValidate.resetValidation(true);
-//   // openPopup(profilePopup);
-// }
-
-// function handleSaveNewProfileData(data) {
-//   userName.textContent = userNameModalFild.value;
-//   userOccupation.textContent = userOccupationModalFild.value;
-//   // closePopup(profilePopup);
-// }
 
 const cardList = new Section(
   {
@@ -80,7 +64,7 @@ function handleCardClick(placeName, placeImage) {
 }
 
 function renderCard(data) {
-  const card = new Card(data, '#templateCard', handleCardClick);
+  const card = new Card(data, cardTemplateSelector, handleCardClick);
   cardList.addItem(card.createCard());
 }
 
---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+export const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__field',
+  submitButtonSelector: '.button_type_submit',
+  inactiveButtonClass: 'button_disabled',
+  inputErrorClass: 'popup__field_type_error',
+  errorClass: 'popup__error_visible',
+};
+
+export const userNameSelector = '.profile__user-name';
+export const userOccupationSelector = '.profile__occupation';
+export const gallerySelector = '.gallery__card-list';
+export const profilePopupSelector = '.popup_type_edit-profile';
+export const newPlacePopupSelector = '.popup_type_add-place';
+export const popupWithImageSelector = '.popup_type_image-scaling';
+export const cardTemplateSelector = '#templateCard';
+export const buttonOpenProfilePopupSelector = '.button_type_edit';
+export const buttonOpenNewPlacePopupSelector = '.button_type_add';
