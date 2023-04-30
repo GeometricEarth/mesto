8dec0a4df101314ec86f9abf524a16691695d52c
GeometricEath <geo.fsv@gmail.com>
2023-04-30 22:36:32 +0300

    style: переименован компоненент попапа подтверждения

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ import Card from '../components/Card.js';
 import FormValidator from '../components/FormValidator.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
-import PopupWithConfirm from '../components/PopupWithConfirm';
+import PopupWithConfirm from '../components/PopupWithConfirmation';
 
 const buttonEditingProfile = document.querySelector(buttonOpenProfilePopupSelector);
 const buttonShowNewPlacePopup = document.querySelector(buttonOpenNewPlacePopupSelector);
